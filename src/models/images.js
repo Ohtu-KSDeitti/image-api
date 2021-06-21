58b51b9185203329d1ecb8cd059078514248c1c7
k0psutin <jkopsutin@gmail.com>
2021-06-21 13:45:11 +0300

    s3 presigned post etc

--- src/models/images.js
+++ src/models/images.js
@@ -1,5 +1,5 @@
 const { TABLENAME, docClient } = require('../config/dynamodb_config')
-const { deleteFromS3 } = require('../utils/s3')
+const { deleteFromS3, getPresignedPostS3 } = require('../utils/s3')
 
 const getUserImages = (id, client = docClient) => {
   const params = {
@@ -53,11 +53,19 @@ const updateUserImages = (id, userImages, client = docClient) => {
 }
 
 const updateProfilePic = async (id, profilePic) => {
-  const userImages = await getUserImages(id)
+  /*
+const userImages = await getUserImages(id)
 
   const updatedUserImage = { ...userImages, profilePic }
 
-  return await updateUserImages(id, updatedUserImage)
+  const result = await updateUserImages(id, updatedUserImage)
+
+  if (!result) {
+    return null
+  }
+  */
+
+  return JSON.stringify(getPresignedPostS3(id, profilePic))
 }
 
 const removeProfilePic = async (id) => {
